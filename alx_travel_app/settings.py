fb486a89a1523dcd2c05fad037071c64d0c88247
antony-gitee <antony.t.gitee@gmail.com>
2025-07-04 22:03:15 +0300

    Update settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -1,1 +1,50 @@
+import os
+import environ
+from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', default=True)
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    ...
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'listings',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    ...
+]
+
+ROOT_URLCONF = 'alx_travel_app.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
 
